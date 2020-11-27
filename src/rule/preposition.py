b51340699abe1bfb09d68d4a4e4f9296c9153d37
AntiD2ta <mtpotro41@gmail.com>
2020-11-27 12:53:29 -0500

    Upgrade Preposition and Rule classes

--- src/rule/preposition.py
+++ src/rule/preposition.py
@@ -1,18 +1,55 @@
 
 class Preposition:
-    def __init__(self, fuz_set):
-        self.set = fuz_set
-
+    def __init__(self, fuz_set, name):
+        self.sets = {name : fuz_set}
+        
     def __and__(self, other):
-        self.set |= other.set
-        return self
+        return AndPreposition(self, other)
 
     def __or__(self, other):
-        self.set &= other.set
-        return self
+        return OrPreposition(self, other)
 
     def __invert__(self):
-        self.set.membership = lambda x: 1 - self.set.membership(x)
+        return NotPreposition(self)
 
-    def __call__(self, value):
-        return self.set.membership(value)
+    def __call__(self, values):
+        for k, v in values.items():
+            if k in self.sets:
+                return self.sets[k].membership(v)
+        raise ValueError('Item not in sets')
+
+    def __iter__(self):
+        return iter(self.sets.items())
+
+    def __len__(self):
+        return len(self.sets)
+
+
+class BinaryPreposition(Preposition):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class AndPreposition(BinaryPreposition):
+    def __call__(self, values):
+        return min(
+            self.left(values),
+            self.right(values),
+        )
+
+
+class OrPreposition(BinaryPreposition):
+    def __call__(self, values):
+        return max(
+            self.left(values),
+            self.right(values),
+        )
+
+
+class NotPreposition(Preposition):
+    def __init__(self, prep):
+        self.prep = prep
+
+    def __call__(self, values):
+        return 1 - self.prep(values)
--- src/rule/rule.py
+++ src/rule/rule.py
@@ -1,6 +1,3 @@
-
-from rule.preposition import Preposition
-
 from .preposition import Preposition
 
 class Rule:
@@ -8,5 +5,17 @@ class Rule:
         self.antecedents = antecedents
         self.consecuents = consecuents
 
-    def __call__(self, value):
-        return self.consecuents(self.antecedents(value))
+    def __call__(self, values):
+        fuzz_value = self.antecedents(values)
+        union = None
+        key = ''
+        for k, fuzz_set in self.consecuents:
+            key = k
+            if union is None:
+                union = fuzz_set
+            else:
+                union |= fuzz_set
+        fuzz_value = self.consecuents({key :fuzz_value})
+
+        return (fuzz_value, union)
+            
